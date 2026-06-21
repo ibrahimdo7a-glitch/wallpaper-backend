5f2eeb4b003548d64a02afab0ceeb79105af4cd5
Ibrahim <ibrahim.do7a@gmail.com>
2026-06-21 14:29:48 +0300

    temp: add debug-last-error route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin');
 });
+
+// Temporary debug route - shows last Laravel error
+Route::get('/debug-last-error', function () {
+    $logFile = storage_path('logs/laravel.log');
+    if (!file_exists($logFile)) return 'No log file found';
+    $content = file_get_contents($logFile);
+    // Get last 5000 chars
+    $tail = substr($content, -5000);
+    return response('<pre>' . htmlspecialchars($tail) . '</pre>');
+});
